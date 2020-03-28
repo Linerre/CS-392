5190a932ff7d92d32f78f33a435af63198e5c2ac
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 13:43:47 -0400

    add compiling/linking comments

--- classwork/libraries/main.c
+++ classwork/libraries/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int cs392_sum(int, int);
 
+// creates the library object
+// gcc -O0 -c cs392_sum.c
+
+// creates the shared library
+// gcc -shared -o libcs392_sum.so cs392_sum.o
+
+// compiles the main program
+// gcc -O0 -c main.c
+
+// links the library and main
+// gcc -O0 -o main main.o cs392_sum.o
+
 int main() {
     int a = 2;
     int b = 3;
